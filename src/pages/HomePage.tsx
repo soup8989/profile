9be77ae128eec8184bb2353acec19ab15f51e21a
caton <catontan7@gmail.com>
2026-07-03 12:55:47 +1000

    tailwind vite setup

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,15 @@
 const HomePage = () => {
   return (
-    <p> Home Page </p>
+    <>
+      <div id="background" className="bg-blue-800 w-screen h-screen">'
+        {/* Clouds */}
+        <div id="main-container" className="mx-auto w-[50vw] h-screen bg-white">
+
+          <h1 className=""> This is the dashboard </h1>
+        </div>
+      </div>
+
+    </>
   );
 }
 
