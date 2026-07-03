f72005691133bb489b6c4a5c2c084504f2cadc19
caton <catontan7@gmail.com>
2026-07-03 14:51:39 +1000

    diagonal animations

--- src/components/CloudLayer.tsx
+++ src/components/CloudLayer.tsx
@@ -1,5 +1,6 @@
 import { Cloud } from '../components/Cloud'
 import cloud1 from "../assets/clouds/cloud1.png";
+import cloud2 from "../assets/clouds/cloud2.png";
 
 const CloudLayer = () => {
   // const config = LAYER_CONFIG[props.layer];
@@ -7,7 +8,7 @@ const CloudLayer = () => {
   return (
 		<>
 			<Cloud image={cloud1} layer='fore'/>
-			<Cloud image={cloud1} layer='mid'/>
+			<Cloud image={cloud2} layer='mid'/>
 			<Cloud image={cloud1} layer='back'/>
 		</>
   )
